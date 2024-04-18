e40a184a0cea147411779661e7fd85da06e9b39a
Nikitkos19 <lovex7458@gmail.com>
2024-04-18 21:12:21 +0300

    0

---
+++ SpringMobileAppServer/src/main/java/org/universam/springmobileappserver/SpringMobileAppServerApplication.java
@@ -0,0 +1,13 @@
+package org.universam.springmobileappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMobileAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMobileAppServerApplication.class, args);
+    }
+
+}
---
+++ SpringMobileAppServer/src/test/java/org/universam/springmobileappserver/SpringMobileAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.universam.springmobileappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMobileAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
