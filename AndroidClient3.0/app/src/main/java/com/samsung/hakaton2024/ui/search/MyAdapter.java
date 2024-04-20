26eb12d41757ba0666b3337ef199f6f094bd8fa7
gorgontt <166929134+gorgontt@users.noreply.github.com>
2024-04-21 00:02:53 +0300

    работает

--- AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/ui/search/MyAdapter.java
+++ AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/ui/search/MyAdapter.java
@@ -1,26 +1,42 @@
 package com.samsung.hakaton2024.ui.search;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.samsung.hakaton2024.R;
+
+import java.util.List;
+
 public class MyAdapter extends RecyclerView.Adapter<ViewHolderCustomDialog> {
 
+    Context context;
+    List<Item> items;
+
+    public MyAdapter(Context context, List<Item> items) {
+        this.context = context;
+        this.items = items;
+    }
 
     @NonNull
     @Override
     public ViewHolderCustomDialog onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ViewHolderCustomDialog(LayoutInflater.from(context).inflate(R.layout.item_of_custom_dialog, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolderCustomDialog holder, int position) {
 
+        holder.text.setText(items.get(position).getGenre());
+        holder.img.setImageResource(items.get(position).getImg());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return items.size();
     }
 }
--- AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/ui/search/SearchFragment.java
+++ AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/ui/search/SearchFragment.java
@@ -1,5 +1,7 @@
 package com.samsung.hakaton2024.ui.search;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,22 +9,86 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.samsung.hakaton2024.R;
 import com.samsung.hakaton2024.databinding.FragmentSearchBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchFragment extends Fragment {
 
     private FragmentSearchBinding binding;
+    private List<Item> itemList;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        SearchViewModel searchViewModel =
-                new ViewModelProvider(this).get(SearchViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        SearchViewModel searchViewModel = new ViewModelProvider(this).get(SearchViewModel.class);
 
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.imgFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFilterDialog();
+            }
+        });
+
+        itemList = new ArrayList<>();
+        // Добавьте элементы в список itemList
+        itemList.add(new Item("hghghgghgg", R.drawable.like));
+        itemList.add(new Item("hghghgghgg", R.drawable.like));
+        itemList.add(new Item("hghghgghgg", R.drawable.like));
+        itemList.add(new Item("hghghgghgg", R.drawable.like));
+        itemList.add(new Item("hghghgghgg", R.drawable.like));
+        itemList.add(new Item("hghghgghgg", R.drawable.like));
+        itemList.add(new Item("hghghgghgg", R.drawable.like));
+        itemList.add(new Item("hghghgghgg", R.drawable.like));
+        itemList.add(new Item("hghghgghgg", R.drawable.like));
+        itemList.add(new Item("hghghgghgg", R.drawable.like));
+        itemList.add(new Item("hghghgghgg", R.drawable.like));
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+
+        View inflatedView = getLayoutInflater().inflate(R.layout.custom_dialog, null);
+        RecyclerView recycler = (RecyclerView) inflatedView.findViewById(R.id.rv_customD);
+
+        recycler.setLayoutManager(layoutManager);
+        MyAdapter myAdapter = new MyAdapter(getContext(), itemList);
+        recycler.setAdapter(myAdapter);
+    }
+
+    private void showFilterDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Выберите жанр");
+        View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.custom_dialog, null);
+        builder.setView(dialogView);
+
+        RecyclerView recyclerView = dialogView.findViewById(R.id.rv_customD);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        MyAdapter myAdapter = new MyAdapter(getContext(), itemList);
+        recyclerView.setAdapter(myAdapter);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.create().show();
     }
 
     @Override
