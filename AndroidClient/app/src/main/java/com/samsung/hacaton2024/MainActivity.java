9776b4cc5c00f0d5475fe86d59b15ec91da5cd56
SobakinAlex <amsabanov@gmail.com>
2024-04-20 12:32:54 +0300

    +icons

--- AndroidClient/app/src/main/java/com/samsung/hacaton2024/MainActivity.java
+++ AndroidClient/app/src/main/java/com/samsung/hacaton2024/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView navMainBottom = findViewById(R.id.nav_main_bottom);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(binding.navMainBottom, navController);
     }
 
 }
